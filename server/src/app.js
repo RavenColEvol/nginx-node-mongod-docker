b31cfe61b3a9c9c08c9ec17859bfa29c2569554a
Ravi Lamkoti <ravilamkoti@gmail.com>
2022-05-16 00:14:09 +0530

    add: docker-compose to setup

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log('Request received here.');
-    next();
-});
 app.use(require('./routes/todo.routes'));
 
 const PORT = process.env.PORT || 3000;
--- server/src/routes/todo.routes.js
+++ server/src/routes/todo.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const os = require('os');
 
 router.get('/todos', (req, res) => {
+    res.setHeader('hostname', os.hostname());
     const db = require('../db').getDb();
     db
     .collection('todos')
